dac84a9285ce37e68a90a1471cc5f7f099e5fe70
Hinal Gandhi <hinal.g@simformsolutions.com>
2023-11-29 17:33:48 +0530

    Added social svgs and config provider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ConfigProvider } from 'antd';
+import { theme } from './config/theme.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider theme={theme}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
 
